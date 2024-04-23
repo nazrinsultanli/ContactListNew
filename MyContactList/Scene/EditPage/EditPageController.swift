e2743e607a8950180bca9776e85a1db53dc731a1
nazrinsultanli <122233725+nazrinsultanli@users.noreply.github.com>
2024-04-23 18:51:09 +0400

    zzzz

--- MyContactList/Scene/EditPage/EditPageController.swift
+++ MyContactList/Scene/EditPage/EditPageController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 
 class EditPageController: UIViewController {
-    
+
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
@@ -28,25 +28,38 @@ class EditPageController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setConstraints()
-        
+        adjustTableViewInsets(size: -80)
+        navigationController?.navigationBar.isHidden = true
+
     }
 
+    
+    func adjustTableViewInsets(size: CGFloat) {
+        let topInset = navigationController?.navigationBar.frame.height ?? 0
+        tableView.contentInset = UIEdgeInsets(top: size, left: 0, bottom: 0, right: 0)
+        tableView.scrollIndicatorInsets = tableView.contentInset
+    }
+
+    
     func setConstraints() {
         view.addSubview(tableView)
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
 
+   
+    
 }
 
 
 extension EditPageController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        2
+        40
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -68,24 +81,24 @@ extension EditPageController: UITableViewDelegate, UITableViewDataSource {
         headerView.backgroundColor = .systemPurple
         return headerView
     }
-
-    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        (view as! UITableViewHeaderFooterView).contentView.backgroundColor = UIColor.red.withAlphaComponent(0.4)
-        (view as! UITableViewHeaderFooterView).textLabel?.textColor = UIColor.white
-    }
+//
+//    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+//        (view as! UITableViewHeaderFooterView).contentView.backgroundColor = UIColor.red.withAlphaComponent(0.4)
+//        (view as! UITableViewHeaderFooterView).textLabel?.textColor = UIColor.white
+//    }
     
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 180
+        return 300
     }
     //MARK: ------
 
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        if scrollView.contentOffset.y < -150 {
-//            
-//        }
-//        print(scrollView.contentOffset.y)
-//    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y < -150 {
+            adjustTableViewInsets(size: 80)
+        }
+        print(scrollView.contentOffset.y)
+    }
 }
 
 
